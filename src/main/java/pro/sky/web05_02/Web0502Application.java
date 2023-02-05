29334c3af4e6e6b5a84a1665f3c687254d183d4d
Дом <ira-rostov80@mail.ru>
2023-02-05 22:45:41 +0300

    первый коммит-1

---
+++ src/main/java/pro/sky/web05_02/Web0502Application.java
@@ -0,0 +1,13 @@
+package pro.sky.web05_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web0502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web0502Application.class, args);
+    }
+
+}
